ca46fa9ce1ffc266e7769164fece4e5713312315
thanhkhon03 <2121050666@student.humg.edu.vn>
2023-03-14 08:20:41 +0700

    Bai2

--- BaiThucHanhLab2/Bai2.java
+++ BaiThucHanhLab2/Bai2.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner6; Scanner;
 public class Bai2 {
-    
+    Public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.print (" Nhập vào một số ");
+            int n = scanner.nextInt();
+        if (n % 2 == 0) {
+            System.out.println(n + "là số chẵn");
+    }else{
+        System.out.println(n+ "là số lẻ");
+    }
+    }
 }
