2aa75cd3b8b5aea202059699aa48514a1ae46267
thanhkhon03 <2121050666@student.humg.edu.vn>
2023-03-14 10:40:22 +0700

    for

--- BaiThucHanhLab2/Bai8.java
+++ BaiThucHanhLab2/Bai8.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class Bai8 {
-    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số lượng phần tử trong dãy: ");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+        for (int i = 0;i < n; i++){
+            System.out.printf("Nhập phần tử thứ %d: ",i + 1);
+            int number =scanner.nextInt();
+            sum +=number;
+
+        }
+        double average = (double)sum /n;
+        System.out.println("Trung bình cộng của dãy số là: %.2f", average);
+        scanner.close();
+
+
+    }
 }
