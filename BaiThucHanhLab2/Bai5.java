d78a1d522e626157337710ffe333445449f3b946
thanhkhon03 <2121050666@student.humg.edu.vn>
2023-03-14 10:03:03 +0700

    while

--- BaiThucHanhLab2/Bai5.java
+++ BaiThucHanhLab2/Bai5.java
@@ -1,3 +1,18 @@
+import java.until.Scanner;
+
 public class Bai5 {
+    public static void main (String[] args){
+        java.util.Scanner scanner = new Scanner (System.int);
+        int sum =0;
+        int num ;
+        
+        while (sum <=100){
+            System.out.println("Nhap vao mot so nguyen:");
+            num = scanner.nextInt();
+            sum += num ;
+
+        }
+        System.out.println("Tong cac so da nhap:" +sum);
+    }
     
 }
