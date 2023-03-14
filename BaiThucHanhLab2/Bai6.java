8f1d0ca0a4bf6a3f5a2cf592fd2acaccbbd69f8a
thanhkhon03 <2121050666@student.humg.edu.vn>
2023-03-14 10:14:38 +0700

    do-while

--- BaiThucHanhLab2/Bai6.java
+++ BaiThucHanhLab2/Bai6.java
@@ -1,3 +1,19 @@
+import java.until.Scanner;
 public class Bai6 {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap so nguyen duong n: ");
+        int n = sc.nextInt();
+        sc close();
+
+        int giaiThua = 1;
+        int i = 1;
+        do{
+            giaiThua *=i;
+            i++;
+
+        } while(i<=n);
+        System.out.println("Giai thua cua "+ n + "la:"+ giaiThua);
+    }
     
 }
