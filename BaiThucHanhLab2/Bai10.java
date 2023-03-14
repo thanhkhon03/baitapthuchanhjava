9d4af20466efaaa7f45a57d0650360ccdf34f744
thanhkhon03 <2121050666@student.humg.edu.vn>
2023-03-14 10:59:52 +0700

    String

--- BaiThucHanhLab2/Bai10.java
+++ BaiThucHanhLab2/Bai10.java
@@ -1,3 +1,25 @@
+import java.until.Scanner;
 public class Bai10 {
+    public static void main(String[] args){
+        java.util.Scanner scanner =new Scanner (System.in);
+
+        //Nhap chuoi va ki tu can tim
+        System.out.print("Nhap chuoi: ");
+        String str =scanner.nextline();
+        System.out.print("Nhap ky tu can tim: ");
+        char ch = scanner.next().charAt(0);
+
+        // Dem so lan xuat hien cua ky tu trong chuoi 
+        int count =0;
+        for (int =0;i < str length();i++){
+            if (str.charAt(i) == ch){
+                count++;
+            }
+        }
+        // In ra ket qua 
+        System.out.println("So lan xuat hien cua ky tu"+ ch + "trong chuoi la: "+count);
+
+        scanner.close();
+    }
     
 }
